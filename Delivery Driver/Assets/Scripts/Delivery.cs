d4ee36e2d28930aadeaa81734b015d79fca06da9
Abid Hussen <abidhussen351@gmail.com>
2025-06-17 18:04:23 +0600

    New Updates

--- Delivery Driver/Assets/Scripts/Delivery.cs
+++ Delivery Driver/Assets/Scripts/Delivery.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);  
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = noPackageColor;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected with: " + collision.gameObject.name);
@@ -13,11 +23,13 @@ public class Delivery : MonoBehaviour
         {
             hasPackage = true;
             Debug.Log("Package Picked Up!");
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject);
         }
         if(collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered!");
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
     }
