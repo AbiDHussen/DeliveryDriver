e51231d70a19ea14d7191a8f4a5a97ce87f95d46
Abid Hussen <abidhussen351@gmail.com>
2025-06-17 19:57:12 +0600

    Completed Delivery Driver Game First Release

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -2,17 +2,31 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float turnSpeed = 0.1f;
-    [SerializeField] float carSpeed = 0.01f;
-    void Start()
-    {
-        //transform.Rotate(0, 0, 45);
-    }
+    [SerializeField] float turnSpeed = 100.0f;
+    [SerializeField] float carSpeed = 10.0f;
+    [SerializeField] float slowSpeed = 15.0f;
+    [SerializeField] float boostSpeed = 30.0f;
+   
     void Update() 
     {
         float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
         float speedAmount = Input.GetAxis("Vertical") * carSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -turnAmount);
         transform.Translate(0, speedAmount, 0);    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        carSpeed = slowSpeed;
+        Debug.Log("speed slowed down!");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Boost")
+        {
+            carSpeed = boostSpeed;
+            Debug.Log("Boost Activated!");
+        }
     }
 }
